8390adb314466de89c3104d16fbfe08cae4663ab
HACKE-RC <60568652+HACKE-RC@users.noreply.github.com>
2020-07-29 05:28:36 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+if [ -d ../usr/bin ];
+then
+	if [ -d webScrap ];
+	then
+		cp webScrap ../usr/bin
+		echo "[-] Process Completed [-]"
+		echo "[+] Now Give Command webScrap To Start Webscrap [+]"
+	else:
+		echo "[?] webScrap Not Found [?]"
+		echo "[?] Try : Clone Again From Github [!]"
+	fi
+else
+	 echo "[?] usr/bin Not Found [?]"
+	 echo "[!] Try Moving webScrap To usr/bin [!]"
+fi
